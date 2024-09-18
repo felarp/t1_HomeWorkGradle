8a27f7034b152caf6b7cd582f7e77216221dfdb7
IgorO <felarp@mail.ru>
2024-09-18 17:49:02 +0400

    Add files via upload

---
+++ api-tests/src/test/java/RunApiTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class RunApiTest {
+
+    @Test
+
+    public void exampleTest() {
+        int a = 2;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+}
